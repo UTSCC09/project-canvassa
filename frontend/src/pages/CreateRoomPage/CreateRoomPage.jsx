5fbb0cf26709e9dcf36159af7c6899b9b54a6e6c
STalukder20 <shammotalukder4@gmail.com>
2022-06-21 14:18:38 -0400

    Create rooms page fe almost done. Widths should be fixed.

--- frontend/src/pages/CreateRoomPage/CreateRoomPage.jsx
+++ frontend/src/pages/CreateRoomPage/CreateRoomPage.jsx
@@ -3,36 +3,39 @@ import styled from "styled-components";
 import { Button, TitleText } from "../../shared/components";
 
 export const CreateRoomPage = () => {
-  return (<Container>
-    <TitleText>Canvassa</TitleText>
-    <ContentContainer>
-      <TitleText style={{ fontSize: "1.5rem" }}>Create a Room</TitleText>
-      <TitleText style={{ fontSize: "1.25rem" }}>
-        Create a private room and invite your friends!
-      </TitleText>
-      <ContentContainer>
-        <TitleText style={{ fontSize: "1.25rem" }}>Normal</TitleText>
-        <TitleText style={{ fontSize: "1.25rem" }}>
-          Have fun drawing with your friends.
+  return (
+    <Container>
+      <TitleText>Canvassa</TitleText>
+      <ContentContainer style={{ background: "#f2f2f2" }}>
+        <TitleText style={{ fontSize: "4rem" }}>Create a Room</TitleText>
+        <TitleText style={{ fontSize: "3rem", color: "#767676" }}>
+          Create a private room and invite your friends!
         </TitleText>
+        <GridContainer>
+          <ContentContainer>
+            <TitleText style={{ fontSize: "2.5rem" }}>Normal</TitleText>
+            <TitleText style={{ fontSize: "2.25rem" }}>
+              Have fun drawing with your friends.
+            </TitleText>
+          </ContentContainer>
+          <ContentContainer>
+            <TitleText style={{ fontSize: "2.5rem" }}>Round Robin</TitleText>
+            <TitleText style={{ fontSize: "2.25rem" }}>
+              Get a prompt and take turns drawing it!
+            </TitleText>
+          </ContentContainer>
+          <ContentContainer>
+            <TitleText style={{ fontSize: "2.5rem" }}>Presenting</TitleText>
+            <TitleText style={{ fontSize: "2.25rem" }}>
+              Have a large audience and few presenters that control the canvas
+            </TitleText>
+          </ContentContainer>
+        </GridContainer>
+        <ButtonContainer>
+          <Button>Back</Button>
+        </ButtonContainer>
       </ContentContainer>
-      <ContentContainer>
-        <TitleText style={{ fontSize: "1.25rem" }}>Round Robin</TitleText>
-        <TitleText style={{ fontSize: "1.25rem" }}>
-          Get a prompt and take turns drawing it!
-        </TitleText>
-      </ContentContainer>
-      <ContentContainer>
-        <TitleText style={{ fontSize: "1.25rem" }}>Presenting</TitleText>
-        <TitleText style={{ fontSize: "1.25rem" }}>
-          Have a large audience and few presenters that control the canvas
-        </TitleText>
-      </ContentContainer>
-      <ButtonContainer>
-        <Button>Back</Button>
-      </ButtonContainer>
-    </ContentContainer>
-  </Container>
+    </Container>
   );
 };
 
@@ -40,7 +43,15 @@ const ButtonContainer = styled.div`
   margin-bottom: 2rem;
 `;
 
-export const ContentContainer = styled.div`
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 200px 200px;
+  grid-row: auto auto;
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+`;
+
+const ContentContainer = styled.div`
   background: #e7e7e7;
   border-radius: 2.5rem;
   padding: 1em;
@@ -51,6 +62,7 @@ export const ContentContainer = styled.div`
   align-items: center;
   flex-direction: column;
   padding: 2rem;
+  width: fit-content;
 `;
 
 const Container = styled.div`
