f55d67ee2a9a2b6a2424feeac884f4951a0fa35a
mcmogaming <mcmogaming@gmail.com>
2022-06-30 07:05:29 -0400

    Fixed Grid Responsive Issue

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,36 +1,39 @@
 import React from "react";
-import styled, {keyframes} from "styled-components";
-import { Button, ContentContainer, TitleText, Markers } from "../../shared/components";
-import backgroundimg from "../../assets/images/math-transparent-background-30.png";
-import {PATHS} from "../../shared/constants";
+import styled, { keyframes } from "styled-components";
 import {
-  useNavigate
-} from "react-router-dom";
-
+  Button,
+  ContentContainer,
+  TitleText,
+  Markers,
+} from "../../shared/components";
+import backgroundimg from "../../assets/images/math-transparent-background-30.png";
+import { PATHS } from "../../shared/constants";
+import { useNavigate } from "react-router-dom";
 
 export const LandingPage = () => {
   const navigate = useNavigate();
-  const goToPublicRoomsPage = ()=> {navigate(PATHS.PUBLIC_ROOMS_PAGE , {replaced:true});};
+  const goToPublicRoomsPage = () => {
+    navigate(PATHS.PUBLIC_ROOMS_PAGE, { replaced: true });
+  };
 
   return (
     <Background>
-    <Container>
-      <TitleText>Canvassa</TitleText>
-      <ContentContainer>
-        <ButtonContainer>
-        <Button onClick={goToPublicRoomsPage}>Public Rooms</Button>
-        </ButtonContainer>
-        <ButtonContainer>
-          <Button>Create a Room</Button>
-        </ButtonContainer>
-        <TitleText style={{ fontSize: "1.5rem" }}>Copyrighted 2022</TitleText>
-      </ContentContainer>
-    </Container>
-    <Markers></Markers>
+      <Container>
+        <TitleText>Canvassa</TitleText>
+        <ContentContainer>
+          <ButtonContainer>
+            <Button onClick={goToPublicRoomsPage}>Public Rooms</Button>
+          </ButtonContainer>
+          <ButtonContainer>
+            <Button>Create a Room</Button>
+          </ButtonContainer>
+          <TitleText style={{ fontSize: "1.5rem" }}>Copyrighted 2022</TitleText>
+        </ContentContainer>
+      </Container>
+      <Markers></Markers>
     </Background>
   );
 };
-
 
 const ButtonContainer = styled.div`
   margin-bottom: 2rem;
@@ -41,7 +44,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-`
+`;
 
 const BackgroundAnimation = keyframes`
     0%   {background-color:rgb(178, 29, 29);   background-size: 1000px;}
@@ -49,10 +52,10 @@ const BackgroundAnimation = keyframes`
     50%  {background-color:rgb(25, 25, 179);background-size: 500px;}
     75%  {background-color:green;background-size: 1500px;}
     100% {background-color:red;background-size: 1000px;}
-`
+`;
 
 const Background = styled.div`
-  height:100vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   flex-direction: column;
