7e3991600eb33493b6adfee056b8e58c4f8d6198
FarhanChowdhury248 <farhanChowdhury248@users.noreply.github.com>
2022-07-01 21:37:18 -0400

    added endpoint to get user profile

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { connectToDb, getDb } = require("./utils/db");
 const { configureAuth } = require("./utils/auth");
+const { requiresAuth } = require("express-openid-connect");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,4 +26,9 @@ configureAuth(app);
 // req.isAuthenticated is provided from the auth router
 app.get("/", (req, res) => {
   res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+// profile details
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user.sub));
 });
